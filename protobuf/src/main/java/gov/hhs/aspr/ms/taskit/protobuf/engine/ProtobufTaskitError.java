bcd7da4273478ae7a2158b1f2c03e7792a37e8f0
Zachary Bischoff <116595361+bischoffz@users.noreply.github.com>
2025-02-10 17:59:45 -0500

    add taskit object (#97)
    * add taskit object
    
    * add testing, add contract exception
    
    * fix broken test

--- protobuf/src/main/java/gov/hhs/aspr/ms/taskit/protobuf/engine/ProtobufTaskitError.java
+++ protobuf/src/main/java/gov/hhs/aspr/ms/taskit/protobuf/engine/ProtobufTaskitError.java
@@ -11,7 +11,10 @@ public enum ProtobufTaskitError implements ContractError {
     INVALID_TRANSLATION_SPEC("Added Translation Specs need to be of parent type Protobuf TranslationSpecs"),
     NULL_FIELD_DESCRIPTOR("The provided field descriptor is null"),
     UNKNOWN_TYPE_URL(
-            "The given type url does not have a corresponding classRef. Either the typeUrl was never provided, or the typeUrl is malformed.");
+            "The given type url does not have a corresponding classRef. Either the typeUrl was never provided, or the typeUrl is malformed."),
+    MALFORMED_TASKIT_OBJECT("The given taskit object does not have any fields set on it"),
+    INVALID_RETRIEVAL("Tried to get value on Taskit Object which was not set"),
+    ;
 
     private final String description;
 
