9d6f82691d9d854269f9e7278330921046cb09f3
bischoffz <bischoffz@leidos.com>
2025-02-10 19:40:28 -0500

    new taskit object design

---
+++ protobuf/src/test/java/gov/hhs/aspr/ms/taskit/protobuf/engine/AT_TaskitObjectHelper.java
@@ -0,0 +1,75 @@
+package gov.hhs.aspr.ms.taskit.protobuf.engine;
+
+import org.junit.jupiter.api.Test;
+
+import com.google.protobuf.Any;
+import com.google.type.Date;
+
+import gov.hhs.aspr.ms.taskit.protobuf.objects.TaskitObjectInput;
+import gov.hhs.aspr.ms.taskit.protobuf.objects.WrapperEnumValue;
+import gov.hhs.aspr.ms.util.annotations.UnitTag;
+import gov.hhs.aspr.ms.util.annotations.UnitTestMethod;
+
+public class AT_TaskitObjectHelper {
+
+    @Test
+    @UnitTestMethod(target = TaskitObjectHelper.class, name = "getValue", args = {
+            TaskitObjectInput.class }, tags = UnitTag.INCOMPLETE)
+    public void testHasObjectVal() {
+
+    }
+
+    @Test
+    @UnitTestMethod(target = TaskitObjectHelper.class, name = "getIntegerTaskitInput", args = { Integer.class }, tags = UnitTag.INCOMPLETE)
+    public void testGetObjectVal() {
+
+    }
+
+    @Test
+    @UnitTestMethod(target = TaskitObjectHelper.class, name = "getAnyTaskitInput", args = { Any.class }, tags = UnitTag.INCOMPLETE)
+    public void testGetIntVal() {
+
+    }
+
+    @Test
+    @UnitTestMethod(target = TaskitObjectHelper.class, name = "getLongTaskitInput", args = { Long.class }, tags = UnitTag.INCOMPLETE)
+    public void testGetDoubleVal() {
+
+    }
+
+    @Test
+    @UnitTestMethod(target = TaskitObjectHelper.class, name = "getFloatTaskitInput", args = { Float.class }, tags = UnitTag.INCOMPLETE)
+    public void testGetBooleanVal() {
+
+    }
+
+    @Test
+    @UnitTestMethod(target = TaskitObjectHelper.class, name = "getDoubleTaskitInput", args = { Double.class }, tags = UnitTag.INCOMPLETE)
+    public void testGetFloatVal() {
+
+    }
+
+    @Test
+    @UnitTestMethod(target = TaskitObjectHelper.class, name = "getDateTaskitInput", args = { Date.class }, tags = UnitTag.INCOMPLETE)
+    public void testGetLongVal() {
+
+    }
+
+    @Test
+    @UnitTestMethod(target = TaskitObjectHelper.class, name = "getStringTaskitInput", args = { String.class }, tags = UnitTag.INCOMPLETE)
+    public void testGetStringVal() {
+
+    }
+
+    @Test
+    @UnitTestMethod(target = TaskitObjectHelper.class, name = "getEnumTaskitInput", args = { WrapperEnumValue.class }, tags = UnitTag.INCOMPLETE)
+    public void testGetDateVal() {
+
+    }
+
+    @Test
+    @UnitTestMethod(target = TaskitObjectHelper.class, name = "getBooleanTaskitInput", args = { Boolean.class }, tags = UnitTag.INCOMPLETE)
+    public void testGetEnumVal() {
+
+    }
+}
